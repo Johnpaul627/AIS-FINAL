8438662b650c6f3b749975a032ee48732cfe2539
pjewellll <princessjewellhernandez>
2026-05-18 23:06:36 +0800

    updated MAY 18, 2026 APPLIED THE LAST ACTIVITY

--- Auth_System/utils/tokenGenerator.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const tokenGenerator = (id) => {
-    const secret = process.env.SECRET || 'ais_dev_secret_2026';
-    return jwt.sign({ id }, secret, { expiresIn: '3d' });
-};
-
-export default tokenGenerator;
